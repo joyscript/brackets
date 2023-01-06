6c901a6fa0535b514bf6a934f1c19036b35e0d48
joyscript <joyscript7@gmail.com>
2023-01-06 20:11:48 +0300

    feat: solve the brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = [];
+  let config = {};
+  for (let item of bracketsConfig) config[item[0]] = item[1];
+
+  for (let bracket of str) {
+    if (!arr.length && !(bracket in config)) return false;
+    if (arr.length && config[arr.at(-1)] == bracket) {
+      arr.pop();
+    } else if (bracket in config) {
+      arr.push(bracket);
+    }
+  }
+
+  return arr.length == 0;
+};
